fd5a00b3946689090ad9b7177575dc5fe014ae9b
Jobaer Islam <156295576+Jobaer-Islam@users.noreply.github.com>
2026-06-17 17:20:07 +0600

    Creating Notes APIs & testing with Postman

--- src/app.js
+++ src/app.js
@@ -4,13 +4,45 @@ const express = require('express');
 
 const app = express()
 
+app.use(express.json())
+
 const notes = []
 
 // title, description
 
-app.post("/notes", (req, res)=>{
+// POST /notes
+app.post("/notes", (req, res) => {
+    notes.push(req.body)
 
-    console.log(req.body);
+    res.status(201).json({
+        message: "Note created successfully"
+    })
+})
+
+app.get("/notes", (req, res) => {
+    res.status(200).json({
+        message: "Notes fetched successfully",
+        notes: notes
+    })
+})
+
+app.delete("/notes/:index", (req, res) => {
+    const index = req.params.index
+    delete notes[index]
+
+    res.status(200).json({
+        message: "note deleted successfully"
+    })
+})
+
+app.patch("/notes/:index", (req, res)=>{
+    const index = req.params.index
+    const description = req.body.description
+    notes[index].description = description
+
+    res.status(200).json({
+        message: "note updated successfully"
+    })
 })
 
 
