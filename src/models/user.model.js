75db6fad83f8b97e299e2d222ef5467a9650867a
Jobaer Islam <156295576+Jobaer-Islam@users.noreply.github.com>
2026-06-27 22:53:50 +0600

    auth done

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,15 +1,32 @@
 const mongoose = require('mongoose');
-
+const { type } = require('node:os');
 
 const userSchema = new mongoose.Schema({
-    username: String,
-    email: {
+
+    username:{
         type: String,
-        unique: true
+        required: true,
+        unique: true,
     },
-    password: String
+    email:{
+        type: String,
+        required: true,
+        unique: true,
+    },
+    password:{
+        type: String,
+        required: true,
+    },
+    role:{
+        type: String,
+        enum: ['user','artist'],
+        default: 'user',
+    },
+
+
 })
 
-const userModel = mongoose.model("user", userSchema)
+const userModel = mongoose.model('user', userSchema)
+
 
 module.exports = userModel;
--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,18 +1,14 @@
 const express = require('express');
-const authController = require("../controllers/auth.controllers")
+const authController = require("../controllers/auth.controller")
 
 const router = express.Router();
 
 
 
-router.post("/register", authController.registerUser )
+router.post('/register', authController.registerUser)
 
-router.get("/test", (req, res)=>{
-    console.log("Cookies:", req.cookies);
-    res.json({
-        message: "Test route",
-        cookies: req.cookies
-    })
-})
+router.post('/login', authController.loginUser)
+
+
 
 module.exports = router;
